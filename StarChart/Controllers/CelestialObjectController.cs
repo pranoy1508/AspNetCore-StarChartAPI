9abb5e3fbad4ecd794814f73502217d7b3b03fa3
pranoy.ghosh <pranoy.ghosh@mindtree.com>
2022-09-15 23:06:01 +0530

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -16,5 +16,42 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet(Name="GetById")]
+        [Route("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialBody = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if(celestialBody==null)
+            {
+                return NotFound();
+            }
+            celestialBody.Satellites = _context.CelestialObjects.FirstOrDefault(x => x.OrbitedObjectId == celestialBody.Id).Satellites;
+            return Ok(celestialBody);
+        }
+
+
+        [HttpGet]
+        [Route("{name:string}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialBody = _context.CelestialObjects.FirstOrDefault(x => x.Name.Equals(name,StringComparison.OrdinalIgnoreCase));
+            if (celestialBody == null)
+            {
+                return NotFound();
+            }
+            _context.CelestialObjects.ToList().ForEach(_ =>
+            {
+                if (_.OrbitedObjectId == celestialBody.Id)
+                    _.Satellites = _.Satellites;
+            });
+            return Ok(celestialBody);
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            return Ok(_context.CelestialObjects);
+        }
     }
 }
